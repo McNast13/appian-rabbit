f4805e1acf293b85079b6baa0de095d87929a175
McNast13 <33765470+McNast13@users.noreply.github.com>
2021-02-09 15:45:14 +0000

    Basic Working MQ Service

---
+++ src/main/java/com/example/messagingrabbitmq/MessagingRabbitmqApplication.java
@@ -0,0 +1,60 @@
+package com.example.messagingrabbitmq;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.core.Message;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class MessagingRabbitmqApplication {
+
+	static final String topicExchangeName = "PigsInBlankets";
+
+	static final String queueName = "Sausage Farm";
+
+	static final String message = "some other message";
+
+
+
+	@Bean
+	Queue queue() {
+		return new Queue(queueName, false);
+	}
+
+	@Bean
+	TopicExchange exchange() {
+		return new TopicExchange(topicExchangeName);
+	}
+
+
+
+	@Bean
+	Binding binding(Queue queue, TopicExchange exchange) {
+		return BindingBuilder.bind(queue).to(exchange).with("cook.pass.#");
+
+	}
+
+//	@Bean
+//	SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
+//			MessageListenerAdapter listenerAdapter) {
+//		SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+//		container.setConnectionFactory(connectionFactory);
+//		container.setQueueNames(queueName);
+//		container.setMessageListener(listenerAdapter);
+//		return container;
+//	}
+//
+//	@Bean
+//	MessageListenerAdapter listenerAdapter(Receiver receiver) {
+//		return new MessageListenerAdapter(receiver, "receiveMessage");
+//	}
+
+	public static void main(String[] args) throws InterruptedException {
+		SpringApplication.run(MessagingRabbitmqApplication.class, args).close();
+	}
+
+}
